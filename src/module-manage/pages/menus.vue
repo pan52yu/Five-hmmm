90fe867a6ebd6b004a1f903a1d9f010589952e55
章这 <447567191@qq.com>
2022-08-12 20:13:02 +0800

    开始做菜单页面开发

--- src/module-manage/pages/menus.vue
+++ src/module-manage/pages/menus.vue
@@ -1,27 +1,38 @@
 <template>
-  <div>
-    菜单管理
+  <div class="menusBox">
+    <el-card>
+      <el-row type="flex" justify="end">
+        <el-button
+          type="success"
+          icon="el-icon-edit"
+          size="small"
+          @click="menusAddClick"
+          >添加菜单</el-button
+        >
+      </el-row>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Menus',
   data () {
-    return {
-
-    }
+    return {}
   },
 
-  created () {
-
-  },
+  created () {},
 
   methods: {
-
+    menusAddClick () {
+      console.log(11)
+    }
   }
 }
 </script>
 
-<style scoped lang='less'>
-
+<style scoped lang="less">
+.menusBox {
+  margin: 20px;
+}
 </style>
