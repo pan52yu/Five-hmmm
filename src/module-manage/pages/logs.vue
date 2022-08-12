36e9d8c5222173c6a6a8f6f5339a65f6a9710c29
zhangmengyang <2655419131@qq.com>
2022-08-12 17:37:08 +0800

    创建了日志

---
+++ src/module-manage/pages/logs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="logs">
+    <el-card>
+      <el-alert title="共100条数据" type="info" show-icon> </el-alert>
+
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="操作类型" width="280">
+        </el-table-column>
+        <el-table-column prop="date" label="操作人" width="300">
+        </el-table-column>
+        <el-table-column prop="date" label="执行结果" width="300">
+        </el-table-column>
+        <el-table-column prop="name" label="操作时间" width="300">
+        </el-table-column>
+        <el-table-column prop="address" label="描述"> </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [],
+    };
+  },
+};
+</script>
+
+<style scoped lang="less">
+.logs {
+  margin: 20px 20px;
+}
+</style>
